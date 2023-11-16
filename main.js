ee184319dbf955341bbeafb0b552ff3278dbacbc
EnricoCirilli <enrico.cirilli@gmail.com>
2023-11-16 19:46:11 +0100

    add: mile stone 2 Visualizzazione dinamica dei mes

--- main.js
+++ main.js
@@ -3,11 +3,23 @@ const app = createApp({
     data() {
         //console.log(catalogo);
         return{
-            players:catalogo
+            players:catalogo,
+            todo_text:'',
+            indexPlayer: 0,
         }
     },
     mounted() {
     },
     methods: {
+        addTodo(){
+            this.players.push({
+                text: this.todo_text,
+            });
+            this.todo_text='';
+        },
+        setActivePlayer(index){
+            this.indexPlayer=index;
+
+        }
     }
 }).mount('#app');

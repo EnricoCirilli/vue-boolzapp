236f1557d0ded5a17eee29272bd972bc1325665d
EnricoCirilli <enrico.cirilli@gmail.com>
2023-11-16 15:45:13 +0100

    add:   <!-- Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto  riga 68 73-->

---
+++ main.js
@@ -0,0 +1,13 @@
+const { createApp } = Vue;
+const app = createApp({
+    data() {
+        //console.log(catalogo);
+        return{
+            players:catalogo
+        }
+    },
+    mounted() {
+    },
+    methods: {
+    }
+}).mount('#app');

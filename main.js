0ddffd1b2ab5d192029d9570a59f114300e69416
EnricoCirilli <enrico.cirilli@gmail.com>
2023-11-17 21:09:04 +0100

    add: milestone2/milstone4(milestone-3 n.a.)

--- main.js
+++ main.js
@@ -6,16 +6,28 @@ const app = createApp({
     data() {
         return {
             players: catalogo,
-            indexPlayers: 0
-            //
+            indexPlayers: 0,
+            searchText: "",
         }
     },
     mounted() {
     },
-    methiods: {
-        setActivePlayer: function(index){
+    methods: {
+         setActivePlayer: function(index){
             this.indexPlayers = index;
-        }
-    }
+      },
+      // funzione che si abbina  (riga 43/44 html ) v-model=" searchText e @keyup="searchContact" e stringa vuota searchText"" e v-show="player.visible", alla ricerca di un contatto 
+      // andiamo a controllare con un ciclo forEach se il nome del contatto è incluso nella stringa di ricerca, se è incluso il contatto è visibile
+      // se non è incluso il contatto non è visibile
+      searchContact: function(){
+        this.players.forEach((player) => {
+            if(player.name.toLowerCase().includes(this.searchText.toLowerCase())){
+                player.visible = true;
+            } else {
+                player.visible = false;
+            }
+        });
+      },
+     },
 
 }).mount("#app")
